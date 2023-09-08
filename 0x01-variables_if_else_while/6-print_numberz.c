5c1745a80a5219aa9a0c3b64454cc7b6d1bc7f70
VOIDWRLDZ <elhamdiguennounianass2000@gmail.com>
2023-09-08 14:10:27 -0700

    6th Task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program prints all single digit numbers of base ten
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
